c9075840e63044834924aa92c2b4755547486b37
Rua427 <ekrua427@naver.com>
2023-07-14 18:12:45 +0900

    first

---
+++ db.tsx
@@ -0,0 +1,13 @@
+
+
+export interface  SuperHeroesDB {
+    id : number,
+    name: string,
+    alterEgo: string,
+}
+
+export const InitSuperHeroesDB : SuperHeroesDB[] = [{
+    id : 0,
+    name: "",
+    alterEgo: "",
+}]
